c9a6883be46767c37c82dd267d9d720908f08363
Masaki Murooka <m-murooka@aist.go.jp>
2023-01-21 23:29:16 +0900

    Make the library header-only.

--- src/CubicHermiteSpline.cpp
+++
@@ -1,7 +0,0 @@
-#include <TrajectoryCollection/CubicHermiteSpline.h>
-
-namespace TrajectoryCollection
-{
-template class CubicHermiteSpline<Eigen::Vector3d>;
-template class CubicHermiteSpline<Eigen::VectorXd>;
-} // namespace TrajectoryCollection
--- src/CubicSpline.cpp
+++
@@ -1,7 +0,0 @@
-#include <TrajectoryCollection/CubicSpline.h>
-
-namespace TrajectoryCollection
-{
-template class CubicSpline<Eigen::Vector3d>;
-template class CubicSpline<Eigen::VectorXd>;
-} // namespace TrajectoryCollection
